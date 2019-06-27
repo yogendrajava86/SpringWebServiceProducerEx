b997a9434a332eb06a09a24b55e9d6856cac74b7
yogendrajava86 <yogendrajava86@gmail.com>
2019-06-27 13:46:54 +0530

    updated

--- src/main/java/com/app/controller/BookController.java
+++ src/main/java/com/app/controller/BookController.java
@@ -3,8 +3,12 @@ package com.app.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.model.Book;
 
 
 
@@ -15,6 +19,15 @@ public class BookController {
 	@GetMapping("/show")
 	public ResponseEntity<String> showBook(){
 		ResponseEntity<String> resp=new ResponseEntity<String>("Welcome to Rest API",HttpStatus.OK);
+		return resp;
+	}
+	
+	@PostMapping("/data")
+	public ResponseEntity<Book> viewInfo(@RequestBody Book bk){
+		
+		bk.setBookAuthor("Mr."+bk.getBookAuthor());
+		
+		ResponseEntity<Book> resp= new ResponseEntity<Book>(bk,HttpStatus.OK);
 		return resp;
 	}
 
